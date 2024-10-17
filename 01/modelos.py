61e4ec45fb666fd42a135f3b108ca2cb0f0c195c
Gabriel <budhisadva@gmail.com>
2024-10-17 11:30:49 -0600

    Prac 01 - terminada

--- 01/modelos.py
+++ 01/modelos.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.datasets import make_classification
 from sklearn.datasets import make_regression
-# from matplotlib import pyplot as plt
 import numpy as np
 
 class Node:
@@ -12,64 +11,77 @@ class Node:
 
     def __str__(self):
         return str(self.out)
-#---------------------------------------
+
 class PreActivation(Node):
     def __init__(self, d:float):
         self.w = np.random.randn(d)
         self.b = np.random.randn()
 
     def forward(self, x:np.array):
-        self.x = x
         self.out = np.dot(x, self.w) + self.b
+        self.x = x
         return self.out
 
     def backward(self, cadena:float):
-        self.grad_w = self.x*cadena
-        self.grad_b = cadena
-        return self.grad_w
+        return self.x*cadena
 
     def fit(self, X, Y, T=100, lr=0.1):
         L = SquaredError()
         for t in range(T):
-            np.random.shuffle(X)
-            grad_acc = np.zeros(len(X[0]))
-            for i, x in enumerate(X):
-                fx = self.forward(x)
-                L((fx,Y[i]))
-                grad_r = self.backward(L.backward())
-                grad_acc += grad_r
-            self.w -= (lr/2)*grad_acc
-            self.b -= lr*self.grad_b
+            grad_w = np.zeros(X.shape[1])
+            grad_b = []
+            loss = []
+            indexes = np.arange(X.shape[0])
+            np.random.shuffle(indexes)
+            for i in indexes:
+                z = self.forward(X[i])
+                loss.append(L((z,Y[i])))
+                parcial = L.backward((z,Y[i]))
+                grad_w += self.backward(parcial)
+                grad_b.append(parcial)
+            R = np.mean(loss)
+            grad_b_R = np.mean(grad_b)
+            grad_w_R = (1/len(X))*grad_w
+            if R == 0:
+                return
+            self.w -= lr*grad_w_R
+            self.b -= lr*grad_b_R
+
 
     def predict(self, X):
         Y = []
         for x in X:
             Y.append(self.forward(x))
         return np.array(Y)
-#---------------------------
+
 class Sigmoide(Node):
     def forward(self, z:float):
         self.out = 1 / (1 + np.exp(-z))
         return self.out
 
-    def backward(self, grad_l:float):
-        self.grad_s = self.out*(1-self.out)
-        return self.grad_s*grad_l
-
     def fit(self, X, Y, T=100, lr=0.1):
-        pre = PreActivation(X.shape[1])
+        linear = PreActivation(X.shape[1])
         L = CrossEntropy()
         for t in range(T):
-            np.random.shuffle(X)
-            grad_acc = np.zeros(len(X[0]))
-            for i, x in enumerate(X):
-                fx = self.forward(pre(x))
-                L((fx,Y[i]))
-                grad_r = pre.backward(self.backward(L.backward()))
-                grad_acc += grad_r
-            pre.w -= lr*grad_acc
-            pre.b -= lr*pre.grad_b
-        self.z = pre
+            grad_w = np.zeros(X.shape[1])
+            grad_b = []
+            loss = []
+            indexes = np.arange(X.shape[0])
+            np.random.shuffle(indexes)
+            for i in indexes:
+                fx = self.forward(linear(X[i]))
+                loss.append(L((fx, Y[i])))
+                delta = L.backward((fx,Y[i]))
+                grad_w += linear.backward(delta)
+                grad_b.append(delta)
+            R = np.mean(loss)
+            grad_b_R = np.mean(grad_b)
+            grad_w_R = (1/len(X))*grad_w
+            if R == 0:
+                return
+            linear.w -= lr*grad_w_R
+            linear.b -= lr*grad_b_R
+        self.z = linear
 
     def predict(self, X):
         Y = []
@@ -77,7 +89,7 @@ class Sigmoide(Node):
         for x in X:
             Y.append(perceptron(self.forward(self.z(x))))
         return np.array(Y)
-# -------------------------
+
 class Perceptron(Node):
     def forward(self, x:float):
         if x > 0.5:
@@ -85,33 +97,32 @@ class Perceptron(Node):
         else:
             self.out = 0
         return self.out
-# -------------------------
+
 class CrossEntropy(Node):
+    def __init__(self):
+        self.v_entropias = []
+
     def forward(self, x:tuple):
         fx, y = x
-        epsilon = 1e-15
+        epsilon = 1e-20
         fx = np.clip(fx, epsilon, 1-epsilon)
         self.out = -(y*np.log(fx)+(1-y)*np.log(1-fx))
-        if y == 0:
-            self.grad_l = 1/(1-fx)
-        else:
-            self.grad_l = -(1/fx)
+        return self.out
 
-    def backward(self):
-        return self.grad_l
-# ---------------------
+    def backward(self, x:tuple):
+        fx, y = x
+        return fx-y
+
 class SquaredError(Node):
     def forward(self, x:tuple):
         fx, y = x
-        epsilon = 1e-18
-        delta = y-fx
-        delta = np.clip(delta, epsilon, 1-epsilon)
-        self.out = pow(delta,2)
-        self.grad_l = (delta)*-2
+        self.out = np.square(y-fx)
+        return self.out
 
-    def backward(self):
-        return self.grad_l
-# ---------------------
+    def backward(self, x:tuple):
+        fx, y = x
+        return -2*(y-fx)
+
 def entrena_modelo_logistico():
     X, Y = make_classification(n_samples=1000, n_features=2, n_redundant=0, n_informative=2, random_state=10)
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
@@ -133,7 +144,7 @@ def entrena_modelo_lineal():
     print(f"R2: {r2}")
 
 def main():
-    np.random.seed()
+    # np.random.seed(42)
     entrena_modelo_logistico()
     entrena_modelo_lineal()
 
