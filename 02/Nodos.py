cdf8ffd01e062469ceb7570152fe6773546beecd
Gabriel <budhisadva@gmail.com>
2024-10-30 03:59:33 -0600

    FFN: Stochastic

--- 02/Nodos.py
+++ 02/Nodos.py
@@ -4,8 +4,8 @@ class Node:
     def __init__(self):
         pass
 
-    def __call__(self, x):
-        return self.forward(x)
+    def __call__(self, *kwargs):
+        return self.forward(*kwargs)
 
     def __str__(self):
         return str(self.out) #Valor num del nodo
@@ -21,64 +21,69 @@ class Linear(Node):
             self.w = np.random.randn(row, column) * 0.01
         self.b = np.array([0.0 for _ in range(row)])
 
-    def forward(self, x):
-        self.out = np.dot(self.w, x)+self.b
+    def forward(self, *kwargs):
+        self.x = kwargs[0]
+        self.out = np.dot(self.w, self.x)+self.b
         return self.out
 
-    def backward(self):
-        pass
+    def backward(self, *kwargs):
+        d = kwargs[0]
+        self.grad_b = d
+        self.m_w = 0
+        self.m_b = 0
+        self.grad_w = np.outer(d, self.x)
+        return np.dot(d, self.w)
 # ReLU
 class ReLU(Node):
-    def forward(self, x):
+    def forward(self, *kwargs):
+        x = kwargs[0]
+        l = []
+        for a in x:
+            if a == 0:
+                a = 1e-300
+            l.append((1/np.abs(a))*np.maximum(0, a))
+        self.derivada = l
         self.out = np.array([np.maximum(0, a) for a in x])
         return self.out
 
-    def backward(self):
-        pass
+    def backward(self, *kwargs):
+        cadena = kwargs[0]
+        self.d = self.derivada*cadena
+        return self.d
 # Tanh
 class Tanh(Node):
-    def forward(self, x):
+    def forward(self, *kwargs):
+        x = kwargs[0]
         self.out = np.tanh(x)
         return self.out
 
-    def backward(self):
-        return 1 - self.out**2
+    def backward(self, *kwargs):
+        cadena = kwargs[0]
+        derivada = 1 - self.out**2
+        self.d = derivada*cadena
+        return self.d
 # Softmax
 class Softmax(Node):
-    def forward(self, x):
-        a = np.array([np.exp(i) for i in x])
-        denominador = np.sum(a)
-        self.out = np.array([np.exp(i)/denominador for i in x])
+    def forward(self, *kwargs):
+        x = kwargs[0]
+        max_x = np.max(x)
+        denominador = np.sum([np.exp(i-max_x) for i in x])
+        l = []
+        for i in x:
+            l.append(np.exp(i-max_x)/denominador)
+        self.out = np.array(l)
         return self.out
 
-    def backward(self):
-        pass
+    def backward(self, *kwargs):
+        fx, y = kwargs
+        self.d = fx-y
+        return self.d
 # CrossEntropy
 class CrossEntropy(Node):
-    def forward(self, x):
-        fx, y = x
+    def forward(self, *kwargs):
+        fx, y = kwargs
         y = int(y)
         epsilon = 1e-20
         fx = np.clip(fx, epsilon, 1-epsilon)
         self.out = -np.log(fx[y])
         return self.out
-
-    def backward(self):
-        return -1/self.out
-
-class Sequential(Node):
-    def __init__(self, *kwargs):
-        self.layers = kwargs
-        self.params = []
-        for layer in self.layers:
-            if layer.params:
-                self.params.append(layer)
-
-    def forward(self, x):
-        actual_val = x
-        for layer in self.layers:
-            actual_val = layer(actual_val)
-        return actual_val
-
-    def __getitem__(self, i):
-        return self.layers[i]
--- 02/redes.py
+++ 02/redes.py
@@ -1,6 +1,7 @@
 from funciones import make_classification, grafica_data, crea_batch
-from sklearn.model_selection import train_test_split
 from Nodos import Linear, Tanh, ReLU, Softmax, CrossEntropy
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 import numpy as np
 
 class FeedForward:
@@ -11,6 +12,7 @@ class FeedForward:
         self.h2 = ReLU()
         self.a3 = Linear(m_salida, m2)
         self.h3 = Softmax()
+        self.L = CrossEntropy()
 
     def __call__(self, x):
         return self.forward(x)
@@ -19,25 +21,58 @@ class FeedForward:
         self.out = self.h3(self.a3(self.h2(self.a2(self.h1(self.a1(x))))))
         return self.out
 
-    def fit(self, X, Y, size=10, lr=0.1, T=100):
-        L = CrossEntropy()
-        n, d = X.shape
-        indexes = np.arange(n)
-        np.random.shuffle(indexes)
-        batches = crea_batch(indexes, size)
-        for batch in batches:
-            for i in batch:
-                fx = self.forward(X[i])
-                loss = L( (fx, Y[i]) )
-                print(fx, int(Y[i]), loss)
+    def backward(self, *kwargs):
+        fx, y = kwargs
+        self.a1.backward(self.h1.backward(self.a2.backward(self.h2.backward(self.a3.backward(self.h3.backward(fx, y))))))
 
+    def adagrad(self, epsilon=1e-8, lr=0.1):
+        self.a3.m_w = self.a3.m_w + np.pow(self.a3.grad_w,2)
+        self.a3.m_b = self.a3.m_b + np.pow(self.a3.grad_b,2)
+        self.a3.w -= (lr/np.sqrt(self.a3.m_w+epsilon)) * self.a3.grad_w
+        self.a3.b -= (lr/np.sqrt(self.a3.m_b+epsilon)) * self.a3.grad_b
+        #
+        self.a2.m_w = self.a2.m_w + np.pow(self.a2.grad_w,2)
+        self.a2.m_b = self.a2.m_b + np.pow(self.a2.grad_b,2)
+        self.a2.w -= (lr/np.sqrt(self.a2.m_w+epsilon)) * self.a2.grad_w
+        self.a2.b -= (lr/np.sqrt(self.a2.m_b+epsilon)) * self.a2.grad_b
+        #
+        self.a1.m_w = self.a1.m_w + np.pow(self.a1.grad_w,2)
+        self.a1.m_b = self.a1.m_b + np.pow(self.a1.grad_b,2)
+        self.a1.w -= (lr/np.sqrt(self.a1.m_w+epsilon)) * self.a1.grad_w
+        self.a1.b -= (lr/np.sqrt(self.a1.m_b+epsilon)) * self.a1.grad_b
+
+    def fit(self, X, Y, size=10, lr=0.1, T=100):
+        n, d = X.shape
+        for t in range(T):
+            indexes = np.arange(n)
+            np.random.shuffle(indexes)
+            batches = crea_batch(indexes, size)
+            for batch in batches:
+                for i in batch:
+                    fx = self.forward(X[i])
+                    loss = self.L(fx, Y[i])
+                    self.backward(fx, Y[i])
+                    self.adagrad()
+
+    def predict(self, X):
+        Y = []
+        for x in X:
+            Y.append(self.forward(x))
+        return np.array(Y)
 
 def main():
     X, Y = make_classification()
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
     n, d = X.shape
-    red = FeedForward(d, 5, 5)
+    red = FeedForward(d, 5, 6)
     red.fit(x_train, y_train)
+    y_pred = red.predict(x_test)
+    tem = []
+    for i in range(len(y_test)):
+        tem.append(y_pred[i][int(y_test[i])])
+    lista = [1 if x > 0.5 else 0 for x in tem]
+    report = classification_report(y_test, lista)
+    print(report)
 
 if __name__ == '__main__':
     main()
